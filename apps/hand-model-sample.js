92adf813cee652a8854fcfa7de467bd7ade89e01
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2021-07-16 02:59:58 +0530

    added awan's code - sphere scene

--- apps/hand-model-sample.js
+++ apps/hand-model-sample.js
@@ -1,125 +1,84 @@
-import randomColor from "randomcolor";
-import * as THREE from "three";
-import { BoxLineGeometry } from "three/examples/jsm/geometries/BoxLineGeometry";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { VRButton } from "three/examples/jsm/webxr/VRButton";
+import * as THREE from 'three';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton';
+import { BoxLineGeometry } from 'three/examples/jsm/geometries/BoxLineGeometry';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-class App {
-    constructor() {
-        const container = document.createElement("div");
-        document.body.appendChild(container);
+class App{
+    constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
 
-        this.clock = new THREE.Clock();
+        this.camera = this.createCamera();
+        this.scene = this.createScene();
+        this.renderer = this.createRenderer();
+        container.appendChild(this.renderer.domElement);
 
-        // Camera
-        this.camera = new THREE.PerspectiveCamera(
-            50,
+        this.addLight();
+
+        const geometry = new THREE.SphereBufferGeometry(0.4, 30, 30);
+        const material = new THREE.MeshStandardMaterial({ color: 0xfdffbf});
+
+        const sphere = new THREE.Mesh(geometry, material);
+
+        this.scene.add(sphere)
+
+        const room = new THREE.LineSegments(
+            new BoxLineGeometry(6, 6, 6, 10, 10, 10),
+            new THREE.LineBasicMaterial({
+                color: "#151515",
+            })
+        );
+
+        this.scene.add(room)
+
+        const controls = new OrbitControls( this.camera, this.renderer.domElement );
+
+        this.setupVR();
+        
+        this.renderer.setAnimationLoop(this.render.bind(this));
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    }
+	render() {   
+        this.renderer.render(this.scene, this.camera);
+    }
+    createCamera(){
+        const camera = new THREE.PerspectiveCamera(
+            60,
             window.innerWidth / window.innerHeight,
             0.1,
             100
         );
-        this.camera.position.set(0, 1.6, 3);
-
-        // Scene
-        this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color("#505050");
-
-        // Ambient Light
-        const ambientLight = new THREE.HemisphereLight("#606060", "#040404");
-        this.scene.add(ambientLight);
-
-        // Directional Light
-        const light = new THREE.DirectionalLight("#ffffff");
-        light.position.set(1, 1, 1).normalize();
+        camera.position.set(0, 0, 3);
+        return camera;
+    }
+    createScene() {
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color( 0xbbddff );
+        return scene;
+    }
+    createRenderer() {
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.outputEncoding = THREE.sRGBEncoding;
+        return renderer;
+    }
+    addLight(){
+        const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+		this.scene.add(ambient);
+        const light = new THREE.DirectionalLight();
+        light.position.set( 0.2, 1, 1);
         this.scene.add(light);
-
-        // Renderer
-        this.renderer = new THREE.WebGLRenderer({ antialias: true });
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        // Make the renderer to occupy the entire window
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
-
-        container.appendChild(this.renderer.domElement);
-
-        this.controls = new OrbitControls(
-            this.camera,
-            this.renderer.domElement
-        );
-        this.controls.target.set(0, 1.6, 0);
-        this.controls.update();
-
-        this.initScene();
-        this.setupVR();
-
-        // We need to bind `this` so that we can refer to the App object inside these methods
-        window.addEventListener("resize", this.resize.bind(this));
-        this.renderer.setAnimationLoop(this.render.bind(this));
     }
-
-    /**
-     * Generates a random number between `min` and `max`
-     * @param {number} min Min value
-     * @param {number} max Max value
-     */
-    random(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
-    /**
-     * Initializes a three.js scene
-     */
-    initScene() {
-        this.radius = 0.08;
-
-        this.room = new THREE.LineSegments(
-            new BoxLineGeometry(6, 6, 6, 10, 10, 10),
-            new THREE.LineBasicMaterial({
-                color: "#808080",
-            })
-        );
-        this.scene.add(this.room);
-
-        const objectGeometry = new THREE.IcosahedronBufferGeometry(
-            this.radius,
-            2
-        );
-
-        for (let i = 0; i < 200; i++) {
-            const objectMaterial = new THREE.MeshLambertMaterial({
-                color: randomColor(),
-            });
-            const object = new THREE.Mesh(objectGeometry, objectMaterial);
-            object.position.x = this.random(-2, 2);
-            object.position.y = this.random(-2, 2);
-            object.position.z = this.random(-2, 2);
-
-            this.room.add(object);
-        }
-    }
-
-    /**
-     * Adds a button to enable VR for supported devices
-     */
     setupVR() {
         this.renderer.xr.enabled = true;
         document.body.appendChild(VRButton.createButton(this.renderer));
-    }
-
-    /**
-     * Handles scene on resizing the window
-     */
-    resize() {
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-    }
-
-    /**
-     * Renders the scene on the renderer
-     */
-    render() {
-        this.renderer.render(this.scene, this.camera);
     }
 }
 
