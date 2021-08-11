c8b1d32624568eb934a196d327e2e60b866f682f
awanshrestha <awanshrestha1@gmail.com>
2021-08-11 18:10:54 +0545

    Restructures the Pinch code

--- apps/index.js
+++ apps/index.js
@@ -6,7 +6,7 @@ export { default as MultipleLayers } from "./simple-video-layers/multiple-layers
 
 // WebXR Hand Inputs
 export { default as SimpleHands }  from "./webxr-hands/simple-hands";
-export { default as HandDrawing }  from "./hand-drawing";
+export { default as HandDrawing }  from "./webxr-hands/hand-pinch-drawing";
 export { default as HandGrabbing } from "./hand-grabbing-three";
 export { default as HandGrabbingDist } from "./hand-grabbing-dist";
 export { default as HandWebXR } from './hand-webxr'; 
